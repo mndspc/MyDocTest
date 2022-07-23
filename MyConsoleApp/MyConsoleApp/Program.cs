fd6d89ed1eb24a308133d20eb56bad4346b48b47
Shoaib <shoaib.ghachi@gmail.com>
2022-07-23 16:03:15 +0530

    first commit

---
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
